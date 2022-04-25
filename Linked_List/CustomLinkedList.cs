5b0f6dea253e2fa814dfab64d4bad9a11be1c8f0
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-26 01:28:02 +0530

    [Pratik] added : Simple Linked List programme

---
+++ Linked_List/CustomLinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    internal class CustomLinkedList
+    {
+        public Node head;
+
+        public void AddLast(int data)
+        {
+            Node NewNode = new Node(data);
+            if(head == null)
+            {
+                head = NewNode;
+            }
+            else
+            {
+                Node LastNode = GetLastNode();
+                LastNode.next = NewNode;
+            }
+            Console.WriteLine("{0} Node added to the Linked-List.", NewNode.data);
+        }
+
+        private Node GetLastNode()
+        {
+            Node temp = head;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Displying Linked-List nodes.");
+            Node temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linked List is Empty.");
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(" "+temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Linked_List/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Data Structure Programme.");
+            Console.WriteLine("Choose any one method\n0 : Basic Linked list (Built-In)\n1 : Linked List");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            switch(input)
+            {
+                case 0:
+                    LinkedList<string> list = new LinkedList<string>();
+                    list.AddLast("Rajkumar");
+                    list.AddLast("Divate");
+                    list.AddFirst("Pratik");
+                    foreach(string item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    break;
+                case 1:
+                    CustomLinkedList LinkedList = new CustomLinkedList();
+                    LinkedList.AddLast(56);
+                    LinkedList.AddLast(30);
+                    LinkedList.AddLast(70);
+                    LinkedList.Display();
+                    break;
+
+                default:
+                    Console.WriteLine("Enter a valid Input.");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
